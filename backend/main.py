22d4d6157462e9af5578450e164bc446a280cf1a
telastrus <10052313+telastrus@users.noreply.github.com>
2019-12-17 17:28:18 -0500

    odds and ends

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,5 @@
-import asyncio, websockets, sys, json
+import asyncio, websockets, sys, json, gpiozero
+from datetime import datetime, timedelta
 from adafruit_motorkit import MotorKit
 
 PASSWORD = sys.argv[-1]
@@ -28,9 +29,15 @@ ACTIONS = {'xcw': lambda s: x(s),
            'zf': lambda s: set_all(s),
            'zb': lambda s: set_all(-s)}
 
+pins = [gpiozero.Button(i) for i in [1, 2, 3, 4, 5, 6]] # TODO: Replace the [1, 2, 3, 4, 5, 6]
+
+
+# Message Codes: o - okay; b - bad password; e - error; a - perform action
+
 async def conn(websocket, path):
     active = []
     auth = False
+    limit_triggered = False
     async for msg in websocket:
         if not auth:
             if msg == 'p' + PASSWORD:
@@ -41,9 +48,15 @@ async def conn(websocket, path):
         elif msg.startswith('a'):
             data = json.loads(msg[1:])
             ACTIONS[data['action']](data['speed'])
-            await asyncio.sleep(data['duration'])
+            end = datetime.now() + timedelta(seconds=data['duration'])
+            while datetime.now() < end:
+                for i in pins:
+                    if i.is_pressed:
+                        limit_triggered = True
+                        break
+                if limit_triggered: break
             set_all(0)
-            await websocket.send('o')
+            await websocket.send('o' if limit_triggered else 'e')
 
 srv = websockets.serve(conn, "localhost", 11337)
 print(f"Starting server with password {PASSWORD}...")
